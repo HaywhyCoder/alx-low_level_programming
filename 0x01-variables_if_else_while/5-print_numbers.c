faa1e8609014d4785ee8e9fb9900513491b4dc83
HaywhyCoder <bakareusman7@gmail.com>
2022-06-03 19:55:25 -0700

    Add new programs

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Prints numbers in base 10
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printc("%d\n", num);
+	return (0);
+}
