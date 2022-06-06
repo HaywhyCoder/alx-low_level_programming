19422dca10edd25372133736052c8369a42a157c
HaywhyCoder <bakareusman7@gmail.com>
2022-06-05 18:46:41 -0700

    Add new program

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,25 +8,19 @@
 
 int main(void)
 {
-	int n1d1, n1d2, n2d1, n2d2; /*n - number, d-digit*/
+	int num1, num2;
 
-	for (n1d1 = 0; n1d1 < 10; n1d1++)
+	for (num1 = 0; num1 < 99; num1++)
 	{
-		for (n1d2 = 0; n1d2 < 9; n1d2++)
+		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
-			putchar((n1d1 % 10) + '0');
-			putchar((n1d2 % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
-		}
-	}
-	for (n2d1 = 0; n2d1 < 10; n2d1++)
-	{
-		for (n2d2 = 0; n2d2 < 9; n2d2++)
-		{
-			putchar((n2d1 % 10) + '0');
-			putchar((n2d2 % 10) + '0');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (n2d1 == 9 && n2d2 == 8)
+			if (num1 == 98 && num2 == 99)
 				continue;
 			putchar(',');
 			putchar(' ');
