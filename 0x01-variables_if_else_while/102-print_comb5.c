b5700c20273d3b1ff4b773afcbea7e1ec03672b8
HaywhyCoder <bakareusman7@gmail.com>
2022-06-05 18:37:29 -0700

    Add new program

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *main - Print all possible combination of two two-digit numbers
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n1d1, n1d2, n2d1, n2d2; /*n - number, d-digit*/
+
+	for (n1d1 = 0; n1d1 < 10; n1d1++)
+	{
+		for (n1d2 = 0; n1d2 < 9; n1d2++)
+		{
+			putchar((n1d1 % 10) + '0');
+			putchar((n1d2 % 10) + '0');
+			puchar(' ');
+		}
+	}
+	for (n2d1 = 0; n2d1 < 10; n2d1++)
+	{
+		for (n2d2 = 0; n2d2 < 9; n2d2++)
+		{
+			putchar((n2d1 % 10) + '0');
+			putchar((n2d2 % 10) + '0');
+
+			if (n2d1 == 9 && n2d2 == 8)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
