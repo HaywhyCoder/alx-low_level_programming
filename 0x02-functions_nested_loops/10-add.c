61ae333e8b2cb1be2098fff986cd221220edefb9
HaywhyCoder <bakareusman7@gmail.com>
2022-06-08 14:20:14 -0700

    Add new code

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *add - adds two integers
+ *@arg1: first number
+ *@arg2: second number
+ *
+ *Return: sum of the two numbers
+ */
+
+int add(int arg1, int arg2)
+{
+	int sum;
+
+	sum = arg1 + arg2;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *print_to_98 - Prints from a number to 98 in order
+ *@n: starting number
+ *
+ */
+
+void print_to_98(int n)
+{
+	int start;
+
+	if (n > 98)
+	{
+		for (start = n; start >= 98; start--)
+		{
+			_putchar((start / 10) + '0');
+			_putchar((start % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (start = n; n <= 98; start++)
+		{
+			_putchar((start / 10) + '0');
+			_putchar((start % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
