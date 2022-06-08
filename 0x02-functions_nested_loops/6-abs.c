829c420c6dbdf9be3d3c442f645175cf278b4c5d
HaywhyCoder <bakareusman7@gmail.com>
2022-06-08 11:52:24 -0700

    Add new code

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_abs - compute the absolute of an integer
+ *@n: The number to be checked
+ *
+ *Return: absolute of the integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+	
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints the last digit of a number
+ *@n: number to be checked
+ *
+ *Return: last digit of the number inputed
+ */
+
+int print_last_digit(int n)
+{
+	last_digit = n % 10;
+	return (last_digit);
+}
