db4db830ec0d00aab8e4d6f53c33f8f0e454a743
HaywhyCoder <bakareusman7@gmail.com>
2022-06-13 16:57:29 -0700

    Add new code

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *puts_half - prints half of a string
+ *@str: string argument
+ */
+
+void puts_half(char *str)
+{
+	int i, n = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		n++;
+	if (n % 2 == 0)
+	{
+		for (i = n / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+	else
+		for (i = (n - 1) / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	_putchar('\n');
+}
