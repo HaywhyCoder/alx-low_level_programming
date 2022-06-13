fa60f2dca3bd67d322b1984d6c4b9016492de122
HaywhyCoder <bakareusman7@gmail.com>
2022-06-13 16:45:59 -0700

    Add new code

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *puts2 - print every other character of a string starting from the first one
+ *@str: string to be printed
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
