ce0ed8083bbea3ce90e4ef2521fd239668bec9f2
HaywhyCoder <bakareusman7@gmail.com>
2022-06-13 08:31:27 -0700

    Add new code

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,10 +8,10 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-viod puts2(char *str);
+void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-itn _atoi(char *s);
+int _atoi(char *s);
 
 #endif
