f02d568099dd83b0f794441631f6ec8a17cbaf80
HaywhyCoder <bakareusman7@gmail.com>
2022-06-16 17:02:06 -0700

    Add new function strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strncat - concatenates two strings using at most n bytes from the src string
+ *@dest: destination string
+ *@src: source string
+ *@n: number of bytes of source string
+ *
+ *Return: pointer to the resulting dest string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = int len = 0;
+
+	while (dest[i++])
+		len++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[len + 1] = src[i];
+	dest[len + i] = '\0';
+
+	return (dest);
+}
