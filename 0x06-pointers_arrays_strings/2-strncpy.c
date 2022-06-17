6c525ac5c972b75c5f92b9a4478fae29c31fa63a
HaywhyCoder <bakareusman7@gmail.com>
2022-06-16 17:53:55 -0700

    Add new code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0;
 
-	for (index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index] != '\0' && index < n; index++)
 		dest[index] = src[index];
 
 	for (; index < n; index++)
