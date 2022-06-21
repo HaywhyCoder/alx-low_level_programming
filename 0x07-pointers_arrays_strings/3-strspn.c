fef3259193e0e497c493fea2f56fb873ee589ae5
HaywhyCoder <bakareusman7@gmail.com>
2022-06-20 19:08:51 -0700

    Add new code

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *_strspn - gets the length of a prefix substring
+ *@s: string to be checked
+ *@accept: the prefix to be counted
+ *
+ *Return: number of bytes of the initial segment s that
+ *contains bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, m = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				m++;
+				break;
+			}
+			else
+				m = 0;
+		}
+		if (m == 0)
+			break;
+	}
+	return (m);
+}
