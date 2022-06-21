cf2663436f610795a8963c2fef2cee27289e0035
HaywhyCoder <bakareusman7@gmail.com>
2022-06-20 18:00:13 -0700

    Add new code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,10 +15,13 @@ char *_strchr(char *s, char c)
 	char *p;
 
 	for (i = 0; s[i] != '\0'; i++)
-		if (*s[i] == c)
+	{
+		if (s[i] == c)
 		{
 			p = &s[i];
-			break;
+			return (p);
 		}
-	return (p);
+	}
+
+	return ('\0');
 }
