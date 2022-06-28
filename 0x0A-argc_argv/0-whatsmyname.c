296deab4f4f3447d4d17ca9b30a9d77ffe85d579
HaywhyCoder <bakareusman7@gmail.com>
2022-06-28 02:19:20 -0700

    Add new code

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argc: argument counter
+ *@argv: argument vector
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
