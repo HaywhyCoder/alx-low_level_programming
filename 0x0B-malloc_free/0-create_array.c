b334e7114988a0a19f667cc67b7591fc356df7f2
HaywhyCoder <bakareusman7@gmail.com>
2022-06-29 09:44:08 -0700

    Add new code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,17 +8,21 @@
  *@size: size of array
  *@c: character
  *
- *Return: pointer to the allocared memory
+ *Return:if size = 0 or function fails: 0 
+ *       pointer to the allocated memory
  */
 
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(size * sizeof(c));
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
 	return (ptr);
 }
