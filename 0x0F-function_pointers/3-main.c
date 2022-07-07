6bd96f39f05db0d721ea69ab7e239bb5557f0901
HaywhyCoder <bakareusman7@gmail.com>
2022-07-07 04:30:11 -0700

    fix code segfault in get_op

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
  *Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int __attribute__((unused)) argc, char **argv)
 {
 	int num1, num2;
 	char *op;
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 	num2 = atoi(argv[3]);
 	op = argv[2];
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if ((get_op_func(op) == NULL) || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
